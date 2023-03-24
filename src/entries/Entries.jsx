4e23c55cd27304378667f6517f4aa36eefe012ee
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-03-24 10:59:56 +0600

    added sorting functionality

--- src/entries/Entries.jsx
+++ src/entries/Entries.jsx
@@ -8,7 +8,7 @@ function Entries() {
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState([]);
   const [count, setCount] = useState(0);
-  const [sortAscending, setSortAscending] = useState(true);
+  // const [sortAscending, setSortAscending] = useState(true);
 
   useEffect(() => {
     fetch("https://api.publicapis.org/entries")
@@ -26,24 +26,26 @@ function Entries() {
     setCurrentPage(entries.slice(count, count + 10));
   }, [count]);
 
-
   if (loading) {
     return <div>Loading data from source......</div>;
   }
 
-  function selectCount(x, y) {
-    if (y === -1) {
-      setCount(count - x);
-    } else {
-      setCount(count + x);
-    }
+  function sortAscending() {
+    entries.sort((a, b) => a.Description.localeCompare(b.Description));
+    setEntries([...entries]);
+  }
+
+  function sortDescending() {
+    entries.sort((a, b) => b.Description.localeCompare(a.Description));
+    setEntries([...entries]);
   }
 
   return (
     <div className="container">
       <div className="header">
         <h2>Displaying 10 elements from a large number of elements</h2>
-        <button>Sort {sortAscending ? "Ascending" : "Descending"} using Category</button>
+        <button onClick={sortAscending}>Sort Ascending</button>
+        <button onClick={sortDescending}>Sort Descending</button>
       </div>
       <div className="list">
         <ul>
@@ -55,11 +57,8 @@ function Entries() {
         </ul>
       </div>
       <div className="footer">
-        <button onClick={selectCount(1, -1)}>{count + 1}</button>
-        <button onClick={selectCount(2, -1)}>{count + 2}</button>
-        <p>.  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .</p>
-        <button onClick={selectCount(1, 1)}>{entries.length / 10 - 1}</button>
-        <button onClick={selectCount(2, 1)}>{entries.length / 10}</button>
+        <button>prev</button>
+        <button>next</button>
       </div>
     </div>
   );
